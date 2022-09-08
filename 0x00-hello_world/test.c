7ed8903760a7efb018f8e59909095c78c5ad1ecf
iamukn <n.u.kingsley@gmail.com>
2022-09-08 17:00:17 +0000

    file updated

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (void)
+{
+    int a;
+    int b;
+    int sum;
+    a = 2;
+    b =3;
+    sum = a * b;
+    printf ("the final result is: %d", sum);
+    return (0);
+
+
+}
