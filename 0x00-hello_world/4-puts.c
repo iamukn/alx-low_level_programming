093dfc6d1b66a8590a941eadd8e6c911f70173bf
iamukn <n.u.kingsley@gmail.com>
2022-09-08 18:30:05 +0000

    file updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+ * main - starts program
+ *
+ * Description: 'prints \"Programming is like building a multilingual puzzle'
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
