2c1d38ebd09eb07169abac33a0bcd091a1cdb62f
iamukn <n.u.kingsley@gmail.com>
2022-09-08 20:23:43 +0000

    file updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,17 @@
  * Return: Always 0 (Success)
  */
 #include <stdio.h>
-int main(void)
+int main()
 {
 char charType;
 int intType;
 long int longInt;
 long long longLong;
 float floatType;
-printf("Size of a char:%zu\n", sizeof(charType));
-printf("Size of an int:%zu\n", sizeof(intType));
-printf("Size of a long int:%zu\n", sizeof(longInt));
-printf("Size of a long long:%zu\n", sizeof(longLong));
-printf("Size of a float:%zu\n", sizeof(floatType));
+printf("Size of a char:%zu byye(s)\n", sizeof(charType));
+printf("Size of an int:%zu byte(s)\n", sizeof(intType));
+printf("Size of a long int:%zu byte(s)\n", sizeof(longInt));
+printf("Size of a long long:%zu byte(s)\n", sizeof(longLong));
+printf("Size of a float:%zu byte(s)\n", sizeof(floatType));
 return (0);
 }
--- 0x00-hello_world/test.c
+++ 0x00-hello_world/test.c
@@ -8,9 +8,16 @@
 #include <stdio.h>
 int main(void)
 {
+	int a;
+	a=1;
+	printf("my age is %d", a);
+	return (0);
+}
+/**
+{
 long int intType;
 double doubleType;
 printf("Size of L_int:%lu\n", sizeof(intType));
 printf("Size of double:%zu\n", sizeof(doubleType));
 return (0);
-}
+}*/
