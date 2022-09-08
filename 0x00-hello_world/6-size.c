e4bab42eaf1d4f56c8965e0477391f25bbfe391f
iamukn <n.u.kingsley@gmail.com>
2022-09-08 20:41:39 +0000

    file updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int b;
 long int c;
 long long int d;
 float e;
-printf("Size of a char:%lu byye(s)\n", sizeof(a));
+printf("Size of a char:%lu byte(s)\n", sizeof(a));
 printf("Size of an int:%lu byte(s)\n", sizeof(b));
 printf("Size of a long int:%lu byte(s)\n", sizeof(c));
 printf("Size of a long long int:%lu byte(s)\n", sizeof(d));
