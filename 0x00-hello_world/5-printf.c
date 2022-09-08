70253e0eb1a1ea5945d2472cddbbbd5d5d131967
iamukn <n.u.kingsley@gmail.com>
2022-09-08 18:46:21 +0000

    file updated

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - this function starts the program
+ *
+ * Description: prints 'with proper grammar, but the outcome is a piece of art'
+ *
+ * Return: Always 0 (success)
+ */
+#include <stdio.h> 
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
--- 0x00-hello_world/test.c
+++ 0x00-hello_world/test.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
-int main (void)
+int main(void)
 {
-    int a;
-    int b;
-    int sum;
-    a = 2;
-    b =3;
-    sum = a * b;
-    printf ("the final result is: %d", sum);
-    return (0);
-
-
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
