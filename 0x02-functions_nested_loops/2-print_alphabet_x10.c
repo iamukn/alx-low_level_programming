9791d8fcb464118ccad6ac2bb26a55c6358294eb
iamukn <n.u.kingsley@gmail.com>
2022-09-13 15:42:27 +0000

    added

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,21 @@
-#include
+#include "main.h"
+/**
+ * print_alphabet_x10  - prototype that initiates the command
+ *
+ * description: prints a -z
+ *
+ * return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+for (i = 0; i <= 10; i++)
+{
+	for (j = 'a'; j <= 'z'; j++)
+	{
+	_putchar(j);
+	}
+	_putchar("\n");
+}
+return (0);
+}
