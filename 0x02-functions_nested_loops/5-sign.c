e0ae71459a0c0f5f21b4a731502156f13a74168b
iamukn <n.u.kingsley@gmail.com>
2022-09-13 16:32:05 +0000

    file added

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-return (1);
 _putchar(43 + 0);
+return (1);
 }
 else if (n == 0)
 {
-return (0);
 _putchar(0 + 48);
+return (0);
 }
 else if (n < 0)
 {
-return (-1);
 _putchar(0 + 45);
+return (-1);
 }
 }
