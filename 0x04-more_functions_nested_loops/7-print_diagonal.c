8d67af94558e3c8c92bf422342ca61d734b0f93c
iamukn <n.u.kingsley@gmail.com>
2022-09-16 21:24:08 +0000

    added

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,15 +14,15 @@ int i, j;
 
 if (n <= 0)
 _putchar('\n');
-else
+
+for (i = 0; i < n; i++)
 {
-for (i = 1; i <= n; ++i)
+for (j = 0; j <= i; j++)
 {
-for (j = 1; j <= i; ++j)
-	_putchar(' ');
-_putchar(92);
+_putchar(' ');
+}
+	_putchar(92);
 _putchar('\n');
 
-}
 }
 }
