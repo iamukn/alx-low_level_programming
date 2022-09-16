833aefc3fdd5eec2a86f7d2032fd765997df3d71
iamukn <n.u.kingsley@gmail.com>
2022-09-16 08:45:06 +0000

    file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies 2 values;
+ *
+ * @a:holds one value
+ *
+ * @b: holds value to be multipled
+ *
+ * Return: always 0;
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
