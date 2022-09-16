666293eea2eacd4d95c5757de1b7a45c739b06e7
iamukn <n.u.kingsley@gmail.com>
2022-09-16 20:18:10 +0000

    1 change made

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ *
+ * @n: serves as a line holder
+ *
+ * Return: Always 0;
+ */
+
+void print_line(int n)
+{
+int i;
+
+for (i = 1; i <= n; i++)
+{
+_putchar(95);
+}
+if (n <= 0)
+{
+_putchar('\n');
+}
+_putchar('\n');
+return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ *
+ * @n: serves as a line holder
+ *
+ * Return: Always 0;
+ */
+
+int main (void)
+{
+	int i;
+int n = 10;
+for (i = 1; i <= n; i++)
+{
+_putchar(95);
+}
+if (n <= 0)
+{
+_putchar('\n');
+}
+_putchar('\n');
+return (0);
+}
+
+
