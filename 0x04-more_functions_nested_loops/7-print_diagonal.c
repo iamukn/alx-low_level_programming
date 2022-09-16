daf50dc0237fa6dd6b72654063455ce1c08a2d7c
iamukn <n.u.kingsley@gmail.com>
2022-09-16 14:42:10 -0700

    new file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * print_diagonal - print a diagonal line
+ * print_diagonal - print a diagonal lline
  *
  * @n: is the number of times the \ character
  *     should be printed
