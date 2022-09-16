e58af371458457b0582fa76a3f21dbf0c2db37c4
iamukn <n.u.kingsley@gmail.com>
2022-09-16 18:54:03 +0000

    added

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,16 +7,15 @@
 
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	while (i < 9)
+	for (i = 0; i <= 9; ++i)
 	{
 
-	if (i != 2 && i != 4)
+	if (i == 2 && i == 4)
 	{
-	_putchar('0' + n);
-	}
-	i++;
+	continue;
+	_putchar(i + 48);
 	}
 	_putchar('\n');
 }
