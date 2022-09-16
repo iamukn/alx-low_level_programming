c2e7518248c96f6a45d8b2bb4f8c4dd83fc2c74a
iamukn <n.u.kingsley@gmail.com>
2022-09-16 00:18:32 +0000

    file added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int c, a, z;
+
+a = 'A';
+b = 'Z';
+c = _isupper(c);
+
+if (c >= a || c <= b )
+{
+return (1);
+}
+else 
+{
+return (0);
+}
+return (0);
+}
