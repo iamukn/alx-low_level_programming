81fa0919cf83d04e571724078b37bed538698c6f
iamukn <n.u.kingsley@gmail.com>
2022-09-16 10:38:09 +0000

    file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -12,12 +12,10 @@ void print_numbers(void)
 int num;
 
 num = 0;
-do 
-{
+do	{
 _putchar(num + 48)
 num++;
-}
-while (num >=0 && num <= 9);
+}	while (num >= 0 && num <= 9);
 _putchar('\n');
 return;
 }
