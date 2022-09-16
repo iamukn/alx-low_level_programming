b522d9ffad966062da9a44cc4d053efdf09db4f4
iamukn <n.u.kingsley@gmail.com>
2022-09-16 10:20:35 +0000

    file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print from 0-9
+ *
+ * Return: 0;
+ */
+
+void print_numbers(void)
+{
+int c;
+
+c = 0;
+for (c; c <= 9; c++)
+{
+_putchar(c);
+}
+return;
+}
---
+++ 0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void print(void)
+{
+	int c = 'a';
+	for (c; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+return;
+}
+
+int main(void)
+{
+print();
+return (0);
+}
+
