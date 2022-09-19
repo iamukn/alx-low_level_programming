546219bc88d12416175a6e2b0612cf502f9f8e70
iamukn <n.u.kingsley@gmail.com>
2022-09-19 22:57:40 +0000

    task 1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string;
+ * @s: string to be calculated
+ * Return: 0;
+ */
+
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; *s != '\0' s++)
+{
+	i++;
+}
+return (i);
+}
