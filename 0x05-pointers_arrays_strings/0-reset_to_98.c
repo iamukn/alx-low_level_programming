2f6676c44688e862ec28516e905ef7e3bb347e54
iamukn <n.u.kingsley@gmail.com>
2022-09-19 22:18:49 +0000

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - resets a pointer to 98
+ *@n: - variable of pointer (iam_ukn)
+ *
+ * Return: 0;
+ */
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+return (0);
+}
