b7b728670c6679e612b3bf6d0ae49c15656421ba
iamukn <n.u.kingsley@gmail.com>
2022-09-19 23:07:27 +0000

    task 1

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _str1en - returns the length of a string;
+ * _strlen - returns the length of a string;
  * @s: string to be calculated
  * Return: 0;
  */
 
-int _str1en(char *s)
+int _strlen(char *s)
 {
 int i;
 
-for (i = 0; *s != '\0' ++s)
+for (i = 0; *s != '\0'; ++s)
 {
 	i++;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,5 @@
 void swap_int(int *a, int *b);
 void _putchar(char c);
 void reset_to_98(int *n);
-int _str1en(char *s); 
+int _strlen(char *s); 
 #endif
