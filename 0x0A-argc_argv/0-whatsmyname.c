1c2ed33cbe8d3e9fd3221662a95ff1a0243b8972
iamukn <n.u.kingsley@gmail.com>
2022-10-02 20:29:45 +0000

    Argc, argv task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- function that prints its name
+ *
+ *@argc: CLI argument count
+ *
+ *@argv: char array for CL argument
+ *
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
