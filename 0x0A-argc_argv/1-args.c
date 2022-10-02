4ffe37e2011ac9ba88d94079d6c9559b522acfa3
iamukn <n.u.kingsley@gmail.com>
2022-10-02 20:53:36 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints args passed to it
+ *
+ *@argc: cli arg count
+ *
+ * @argv: cli arg array
+ *
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc -1);
+return (0);
+}
