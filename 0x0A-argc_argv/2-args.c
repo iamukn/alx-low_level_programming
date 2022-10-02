86bb14146120512c195af28decd894a735900231
iamukn <n.u.kingsley@gmail.com>
2022-10-02 21:18:46 +0000

    hot fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - prints all arguments received
  *
@@ -13,7 +14,9 @@ int main(int argc, char *argv[])
 {
 int i;
 
-for (i = 0; i <= argc; i++)
+for (i = 0; i < argc; i++)
+{
 printf("%s\n", argv[i]);
+}
 return (0);
 }
