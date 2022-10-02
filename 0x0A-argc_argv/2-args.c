4121fa8321a58a5bda9e0fab100ea5fe2d0fc832
iamukn <n.u.kingsley@gmail.com>
2022-10-02 21:15:04 +0000

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments received
+ *
+ * @argc: cli arg counts
+ *
+ * @argv: cli arg stored as arrays
+ *
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i <= argc; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
