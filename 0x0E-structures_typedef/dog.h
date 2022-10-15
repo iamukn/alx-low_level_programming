cc39525d9f96f729fd7a74cd3e63118c581e47dd
iamukn <n.u.kingsley@gmail.com>
2022-10-15 16:11:51 +0000

    hottest fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - dog
+ *
+ * @name: name
+ * @age: age
+ * @owner:owner
+ *
+ * Description: this is a struct for dog
+ */
+
 struct dog
-{ 
-char * name;
+{
+char *name;
 float age;
-char * owner;
+char *owner;
 };
 #endif /* ifndef DOG_H */
