e10465cbfcd3ffe8a49fffb1abb8f324f9a9ab3e
iamukn <n.u.kingsley@gmail.com>
2022-10-15 17:07:36 +0000

    files added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,9 @@
 #define DOG_H
 /**
  * struct dog - dog
- *
  * @name: name
  * @age: age
  * @owner:owner
- *
  * Description: this is a struct for dog
  */
 
@@ -16,4 +14,10 @@ char *name;
 float age;
 char *owner;
 };
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d)
 #endif /* ifndef DOG_H */
