9ce912b711e55c6857ac95ab398a7de3fd00514d
iamukn <n.u.kingsley@gmail.com>
2023-04-13 22:41:22 +0000

    hot fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,15 +10,16 @@
 int linear_search(int *array, size_t size, int value)
 {
 
+size_t i;
 if (array == NULL)
 {
 return (-1);
 }
-for (int i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
+printf("Value checked array[%li] = [%i]\n", i, array[i]);
 if (array[i] == value)
 return (i);
 }
 return (-1);
-}
 }
